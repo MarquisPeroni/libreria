0ef45d39f3f681fa2d91ba110d2a460f06fda75f
MarquisPeroni <marco_p2009@live.it>
2024-03-12 17:47:06 +0100

    second.commit

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -6,27 +6,26 @@ import Col from 'react-bootstrap/Col'
 import items from '../data/fantasy.json'
 
 function AllTheBooks() {
-    items.map((book, i) => {
     return (
-    <Container>
+    <Container className='fluid'>
         <Row>
-            <Col xs={12} md={8} lg={6}>
-                <Card key={book.id} style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text >
-                    <Button variant="dark">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-            </Col>
+            {items.map((book, index) => (
+                <Col xs={12} md={6} lg={3} key={index}>
+                    <Card style={{ minHeight: '100%', display: 'flex', flexDirection: 'column' }}>
+                    <Card.Img variant="top" src={book.img} alt={book.title} />
+                    <Card.Body>
+                        <Card.Title >{book.title}</Card.Title>
+                        <Card.Text>
+                            <strong>Price:</strong> {book.price} $
+                        </Card.Text >
+                        <Button variant="dark">Go somewhere</Button>
+                    </Card.Body>
+                    </Card>
+                </Col>
+            ))}
         </Row>
     </Container>
     )
-});
 }
 
 export default AllTheBooks;
